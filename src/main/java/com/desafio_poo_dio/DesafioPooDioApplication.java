1a5200999eaab2643d06ce3e60a98333b9f16d7a
tatiane <tatiane.andrade@wehandle.com.br>
2024-08-05 20:47:36 -0300

    implementando execução dos metodos

--- src/main/java/com/desafio_poo_dio/DesafioPooDioApplication.java
+++ src/main/java/com/desafio_poo_dio/DesafioPooDioApplication.java
@@ -1,9 +1,32 @@
 package com.desafio_poo_dio;
 
+import com.desafio_poo_dio.funcionalidadesIphone.AparelhoTelefonico;
+import com.desafio_poo_dio.funcionalidadesIphone.Navegador;
+import com.desafio_poo_dio.funcionalidadesIphone.ReprodutorMusical;
+import com.desafio_poo_dio.funcionalidadesIphone.impl.IPhone;
+
 public class DesafioPooDioApplication {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		AparelhoTelefonico telefone = new IPhone();
+		telefone.ligar("(27)90000-0000");
+		telefone.iniciarCorreioDeVoz();
+		telefone.atender();
+		System.out.println();
+		
+		Navegador navegador = new IPhone();
+		navegador.exibirPagina("www.google.com");
+		navegador.adicionarNovaAba();
+		navegador.atualizarPagina();
+		System.out.println();
+		
+		ReprodutorMusical reprodutorMusical = new IPhone();
+		reprodutorMusical.selecionarMusica("Tempo Perdido");
+		reprodutorMusical.tocar();
+		reprodutorMusical.pausar();
+		
 
 	}
 }
+
